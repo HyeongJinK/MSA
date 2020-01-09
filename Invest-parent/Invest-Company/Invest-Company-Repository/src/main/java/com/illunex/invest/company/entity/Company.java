ff8a587d38580c0ce52eaeb409ab07bc58892275
김형진 <wkzkfmxk12@illunex.com>
2020-01-09 19:50:41 +0900

    ref #8 Entity 작성중

--- Invest-parent/Invest-Company/Invest-Company-Repository/src/main/java/com/illunex/invest/company/entity/Company.java
+++ Invest-parent/Invest-Company/Invest-Company-Repository/src/main/java/com/illunex/invest/company/entity/Company.java
@@ -3,6 +3,7 @@ package com.illunex.invest.company.entity;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -10,8 +11,21 @@ public class Company {
     @Id
     Long companyIdx;
     Long userIdx;
-    String name;
-    String corporateNumber;
+    String logo;                // 로고
+    String name;                // 이름
+    String businessNumber;      // 사업자 등록번호
+    String companyType;         // 산업분야    // todo enum 혹은 따로 DB 구성 여부
+    Date establishmentDate;     // 설립일
+    String employeeCount;       // 직원수
+    String business;            // 기업구분  todo 스타트업, 중소기업, 중견기업, 대기업..... enum 사용 여부 확인
+    String nation;              // 국가 todo 국가 타입 enum 고려
+    String stocksList;          // 상장구분 todo 미상장, 코스닥, 코스피, enum
+    String zipCode;             // 우편번호
+    String address;             // 주소
+    String addressDetail;       // 상세주소
+    String mainProductLine;     // 주요제품군
+    String description;         // 기업개요
+    String homePage;            // 홈페이지
 
     @OneToMany
     List<CompanyProduct> companyProducts;
