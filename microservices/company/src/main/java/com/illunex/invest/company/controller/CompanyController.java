c4622c2adde70d19774c5b7f1fbc37b1c7016868
김형진 <wkzkfmxk12@illunex.com>
2020-02-04 17:29:47 +0900

    개발 환경 설정&템플릿 작업 에러 처리 추가

--- microservices/company/src/main/java/com/illunex/invest/company/controller/CompanyController.java
+++ microservices/company/src/main/java/com/illunex/invest/company/controller/CompanyController.java
@@ -1,9 +1,12 @@
 package com.illunex.invest.company.controller;
 
+import com.illunex.invest.company.exception.TestException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.illunex.invest.common.exception.exceptions.TemplateException;
 
 @RestController
 public class CompanyController {
@@ -12,5 +15,26 @@ public class CompanyController {
     @RequestMapping("/test")
     public String test() {
         return "test";
+    }
+
+    @RequestMapping("/errorTemplate")
+    public String errorTemplate() {
+        if (true) {
+            throw new TestException("에러 템플릿 테스트 t");
+        }
+        return "test";
+    }
+
+    @RequestMapping("/errorTemplate2")
+    public String CommonErrorTemplate() {
+        if (true) {
+            throw new TemplateException("에러 템플릿 테스트 t2");
+        }
+        return "test";
+    }
+
+    @ExceptionHandler(TestException.class)
+    public String testException() {
+        return "testError";
     }
 }
