0a5d0147e2a803aabe4baa29c755e5ab6eb9f1e3
김형진 <wkzkfmxk12@illunex.com>
2020-02-05 16:05:58 +0900

    TO-172

---
+++ microservices/investorRelations/src/test/java/com/illunex/invest/InvestorRelations/InvestorRelationsEntityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.illunex.invest.InvestorRelations;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvestorRelationsEntityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
