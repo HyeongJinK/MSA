4bc39d7ce533d6d051e5e49089e32ba35742c6a3
dhpark <dhpark@illunex.com>
2020-02-05 18:34:03 +0900

    merge

--- microservices/notice/src/main/java/com/illunex/invest/notice/service/NoticeServiceProxy.java
+++ microservices/notice/src/main/java/com/illunex/invest/notice/service/NoticeServiceProxy.java
@@ -2,11 +2,11 @@ package com.illunex.invest.notice.service;
 
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "gateway")
+//@FeignClient(name = "gateway")
 @RibbonClient(name = "notice")
 public interface NoticeServiceProxy {
     @RequestMapping(value = "/notice/test", method = RequestMethod.GET)
