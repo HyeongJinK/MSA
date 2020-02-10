d835c77ecfc8e0c817b36451252f62b958a4db67
dhpark <dhpark@illunex.com>
2020-02-10 17:20:27 +0900

    TO-82 swagger 활성화, 공지사항 검색, 페이지네이션 구현

--- microservices/board/src/main/java/com/illunex/invest/board/controller/BoardControllerImpl.java
+++ microservices/board/src/main/java/com/illunex/invest/board/controller/BoardControllerImpl.java
@@ -2,18 +2,15 @@ package com.illunex.invest.board.controller;
 
 import com.illunex.invest.api.core.board.controller.BoardController;
 import com.illunex.invest.api.core.board.dto.BoardDto;
-import com.illunex.invest.board.persistence.entity.Board;
 import com.illunex.invest.board.service.BoardService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import org.springframework.data.domain.Pageable;
-import java.util.List;
 
 @RestController
 public class BoardControllerImpl implements BoardController {
@@ -26,11 +23,6 @@ public class BoardControllerImpl implements BoardController {
     @GetMapping("/notice/list")
     @Override
     public ResponseEntity<Page<BoardDto>> getAllPost(@RequestParam Long boardIdx, @RequestParam String subject, Pageable pageable) {
-        System.out.println("---get All Post ---");
-        System.out.println(boardIdx);
-        System.out.println(subject);
-        System.out.println(pageable);
-
         return new ResponseEntity(boardService.getAllPost(boardIdx, subject, pageable), HttpStatus.OK);
     }
 
--- microservices/board/src/main/java/com/illunex/invest/board/service/BoardService.java
+++ microservices/board/src/main/java/com/illunex/invest/board/service/BoardService.java
@@ -1,19 +1,18 @@
 package com.illunex.invest.board.service;
 
+import com.illunex.invest.api.core.board.dto.BoardDto;
 import com.illunex.invest.board.mapper.BoardMapper;
 import com.illunex.invest.board.persistence.entity.Board;
 import com.illunex.invest.board.persistence.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import com.illunex.invest.api.core.board.dto.BoardDto;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import org.springframework.data.domain.Pageable;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 public class BoardService {
@@ -22,20 +21,9 @@ public class BoardService {
     BoardRepository boardRepository;
 
     public Page<BoardDto> getAllPost(@RequestParam Long boardIdx, @RequestParam String subject, Pageable pageable) {
-        System.out.println("-----service----");
-        System.out.println(boardIdx);
-        System.out.println(subject);
-        System.out.println(pageable);
         Page<Board> allPost = boardRepository.findAllByBoardIdxAndSubjectContaining(boardIdx, subject, pageable);
-        System.out.println("-----allPost----");
-        System.out.println(allPost.getContent());
-        System.out.println(allPost.getTotalPages());
-        System.out.println(allPost.getTotalElements());
-
-
-        Page<BoardDto> allPost2 = new PageImpl<BoardDto>(BoardMapper.MAPPER.entityListToDtoList(allPost.getContent()), allPost.getPageable(), allPost.getTotalElements());
-        //Page<BoardDto> allPostDto = BoardMapper.MAPPER.entityListToDtoList(allPost);
-        return allPost2;
+        Page<BoardDto> allPostDto = new PageImpl<BoardDto>(BoardMapper.MAPPER.entityListToDtoList(allPost.getContent()), allPost.getPageable(), allPost.getTotalElements());
+        return allPostDto;
     }
 
     public BoardDto getOnePost(BoardDto boardDto) {
