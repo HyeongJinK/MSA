e77100b185010837e1c9c5b30ed530552b7b1ae2
김형진 <wkzkfmxk12@illunex.com>
2020-02-18 18:53:02 +0900

    유저 프로젝트 설정

--- microservices/company/src/main/java/com/illunex/invest/company/exception/NoneCompanyException.java
+++ microservices/company/src/main/java/com/illunex/invest/company/exception/NoneCompanyException.java
@@ -7,12 +7,4 @@ public class NoneCompanyException extends RuntimeException {
     public NoneCompanyException(String message) {
         super(message);
     }
-
-    public NoneCompanyException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public NoneCompanyException(Throwable cause) {
-        super(cause);
-    }
 }
