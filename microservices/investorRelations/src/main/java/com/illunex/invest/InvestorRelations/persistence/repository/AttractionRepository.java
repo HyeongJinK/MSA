76a8d3979df1d3e4ab011ce573df7a100d86f2ee
dhpark <new0771@naver.com>
2020-02-27 10:55:48 +0900

    02-27 init

--- microservices/investorRelations/src/main/java/com/illunex/invest/InvestorRelations/persistence/repository/AttractionRepository.java
+++ microservices/investorRelations/src/main/java/com/illunex/invest/InvestorRelations/persistence/repository/AttractionRepository.java
@@ -1,10 +1,10 @@
 package com.illunex.invest.InvestorRelations.persistence.repository;
 
-import com.illunex.invest.InvestorRelations.persistence.entity.IREntity;
+import com.illunex.invest.InvestorRelations.persistence.entity.AttractionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AttractionRepository extends JpaRepository<IREntity, Long> {
+public interface AttractionRepository extends JpaRepository<AttractionEntity, Long> {
 //    List<AttractionEntity> findAllByBasicInfoEntityIdx(Long idx);
 }
--- microservices/investorRelations/src/main/java/com/illunex/invest/InvestorRelations/persistence/repository/SubsidyRepository.java
+++ microservices/investorRelations/src/main/java/com/illunex/invest/InvestorRelations/persistence/repository/SubsidyRepository.java
@@ -1,11 +1,10 @@
 package com.illunex.invest.InvestorRelations.persistence.repository;
 
-import com.illunex.invest.InvestorRelations.persistence.entity.AttractionEntity;
-import com.illunex.invest.InvestorRelations.persistence.entity.IREntity;
+import com.illunex.invest.InvestorRelations.persistence.entity.SubsidyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubsidyRepository extends JpaRepository<IREntity, Long> {
-    AttractionEntity findByBasicInfoEntityIdx(Long idx);
+public interface SubsidyRepository extends JpaRepository<SubsidyEntity, Long> {
+
 }
