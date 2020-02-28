76f9ca3c208f902a244d67bda030dd41e8f3e296
dhpark <new0771@naver.com>
2020-02-28 10:18:18 +0900

    merge

--- microservices/investorRelations/src/main/java/com/illunex/invest/InvestorRelations/persistence/entity/ShareholderEntity.java
+++ microservices/investorRelations/src/main/java/com/illunex/invest/InvestorRelations/persistence/entity/ShareholderEntity.java
@@ -10,13 +10,12 @@ import javax.persistence.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "shareholder_status")
-public class ShareHolderStatusEntity {
+@Table(name = "shareholder")
+public class ShareholderEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long idx;
     String name;                // 주주명
     String value;               // 금액
-    String equityRatio;         // 지분율
     String etc;                 // 비고
 }
