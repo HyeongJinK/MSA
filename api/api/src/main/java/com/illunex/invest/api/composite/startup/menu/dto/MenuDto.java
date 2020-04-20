5b4d276eb74c13752609e2b21c4cd286331fa1a1
napasun <wkzkfmxk23@gmail.com>
2020-04-20 17:38:19 +0900

    메뉴 목록 수정

--- api/api/src/main/java/com/illunex/invest/api/composite/startup/menu/dto/MenuDto.java
+++ api/api/src/main/java/com/illunex/invest/api/composite/startup/menu/dto/MenuDto.java
@@ -50,38 +50,64 @@ public class MenuDto {
                 .build();
     }
 
+    private Menu makeSubMenu(String title, String id, String to, List<Menu> subMenus) {
+        return Menu.builder()
+                .title(title)
+                .id(id)
+                .to(to)
+                .subMenu(subMenus)
+                .build();
+    }
+
     private List<Menu> initDashBoardSubMenu(Collection<RoleDTO> roles) {
         return List.of(makeSubMenu("대시보드", "main", "/dashboard/main"));
     }
 
+    private List<Menu> makeInvestSubMenu(RoleDTO role) {
+        return List.of(makeSubMenu("심사", "evaluate", "/invest/evaluate"),
+                makeSubMenu("라운드공지", "round", "/invest/round/list"
+                        , List.of(makeSubMenu("전체", "list", "/invest/round/list"),
+                                makeSubMenu("진행중", "proceeding", "/invest/round/proceeding"),
+                                makeSubMenu("마감", "end", "/invest/round/end"))
+                ),
+                makeSubMenu("설정", "setting", "/invest/setting"));
+    }
+
     private List<Menu> initAlarmSubMenu(Collection<RoleDTO> roles) {
-        return List.of(Menu.builder().build());
+        return List.of(makeSubMenu("전체알람", "alarm", "/myPage/alarm/list"),
+                makeSubMenu("온라인심사", "evaluate", "/myPage/alarm/evaluate"),
+                makeSubMenu("포트폴리오 연결", "portfolio", "/myPage/alarm/portfolio"));
     }
 
     private List<Menu> initAccountSubMenu(Collection<RoleDTO> roles) {
         List<Menu> accountSubMenu = new ArrayList<>();
-        accountSubMenu.add(makeSubMenu("비밀번호 변경", "myPage", "/myPage/password"));
+        accountSubMenu.add(makeSubMenu("프로필 수정", "myPage", "/myPage/account/profile"));
+        accountSubMenu.add(makeSubMenu("비밀번호 변경", "password", "/myPage/account/password"));
+        accountSubMenu.add(makeSubMenu("서명관리", "signature", "/myPage/account/signature"));
 
         roles.stream()
                 .filter(role -> role.getName().equals("ROLE_COMPANY_ADMIN"))
                 .forEach(role -> {
-                    accountSubMenu.add(makeSubMenu("권한설정", "permission", "/myPage/permission"));
-                    accountSubMenu.add(makeSubMenu("서명관리", "signature", "/myPage/signature"));
+                    accountSubMenu.add(makeSubMenu("권한설정", "permission", "/myPage/account/permission/list"));
+                    accountSubMenu.add(makeSubMenu("사업자등록 인증", "business", "/myPage/account/business"));
                 });
 
         return accountSubMenu;
     }
 
     private List<Menu> initPluginSubMenu(Collection<RoleDTO> roles) {
-        return List.of(makeSubMenu("전체 플러그인", "plugin", "/myPage/plugin"));
+        return List.of(makeSubMenu("전체 플러그인", "plugin", "/myPage/plugin/list"));
     }
 
     private List<Menu> initLicenseSubMenu(Collection<RoleDTO> roles) {
-        return List.of(makeSubMenu("결제", "license", "/myPage/license"),
-                makeSubMenu("고객센터", "support", "/myPage/license/cs"));
+        return List.of(makeSubMenu("요금안내", "license", "/myPage/license/payment"),
+                makeSubMenu("고객센터", "support", "/myPage/license/support"));
     }
 
-
+    private List<Menu> initConnectSubMenu(Collection<RoleDTO> roles) {
+        return List.of(makeSubMenu("투자사연결", "connect", "/myPage/connect/vc"),
+                makeSubMenu("팀원연결", "member", "/myPage/connect/member"));
+    }
 
     @NotNull
     private List<Menu> makeCompanySubMenu(Collection<RoleDTO> roles) {
@@ -147,18 +173,17 @@ public class MenuDto {
         boolean companyMenu = true;
         makeDefaultMainMenu(roles);
 
-        if (main != null) {
-            makeMainMenu("기업", "commpany", "company", makeCompanySubMenu(roles));
-        }
-
         roles.stream()
                 .forEach(role -> {
                     switch (role.getName()) {
                         case "ROLE_COMPANY":
-                            makeMainMenu("기업", "commpany", "company", makeCompanySubMenu(roles));
+                            makeMainMenu("기업", "company", "company", makeCompanySubMenu(roles));
                             break;
                         case "ROLE_IR":
                             makeMainMenu("IR 작성", "ir", "ir", makeIrSubMenu(role, irList));
+                            break;
+                        case "ROLE_INVEST":
+                            makeMainMenu("심사", "invest", "invest", makeInvestSubMenu(role));
                             break;
                         case "ROLE_FEED":
                             makeMainMenu("피드", "feed", "feed", List.of(
@@ -185,9 +210,12 @@ public class MenuDto {
                     switch (role.getName()) {
                         case "ROLE_COMPANY_ADMIN":
                             makeMyPageMenu("플러그인", "plugin", "plugin", initPluginSubMenu(roles));
+                            makeMyPageMenu("연결관리", "connect", "plugin", initConnectSubMenu(roles));
                             makeMyPageMenu("라이센스", "license", "license", initLicenseSubMenu(roles));
                             break;
                     }
                 }));
     }
+
+
 }
