f9a146dee46e23c2d0f2780894889580e31a4a5d
dhpark <dhpark@illunex.com>
2020-05-08 15:48:53 +0900

    관심기업 리스트 비동기 변경

--- microservices/vc-composite/src/main/java/com/illunex/invest/vc/service/investment/FavoriteCompanyCompositeIntegration.java
+++ microservices/vc-composite/src/main/java/com/illunex/invest/vc/service/investment/FavoriteCompanyCompositeIntegration.java
@@ -2,22 +2,22 @@ package com.illunex.invest.vc.service.investment;
 
 import com.illunex.invest.api.core.company.dto.VcFavoriteCompanyListDTO;
 import com.illunex.invest.api.core.investment.FavoriteCompanyDTO;
-import com.illunex.invest.api.core.investment.dto.EvaluateStateDTO;
 import com.illunex.invest.api.core.investment.dto.EvaluateStateListDTO;
 import com.illunex.invest.vc.service.DefaultIntegrationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.List;
+import reactor.core.publisher.Mono;
 
 @Component
 public class FavoriteCompanyCompositeIntegration extends DefaultIntegrationService {
     Logger logger = LoggerFactory.getLogger(FavoriteCompanyCompositeIntegration.class);
+
+    @Autowired
+    WebClient.Builder webClientBuilder;
 
     public FavoriteCompanyCompositeIntegration(RestTemplate restTemplate, WebClient.Builder loadBalanceWebClientBuilder) {
         super(restTemplate, loadBalanceWebClientBuilder);
@@ -27,16 +27,24 @@ public class FavoriteCompanyCompositeIntegration extends DefaultIntegrationServi
     private final String investmentUrl = "http://investment";
 
     public FavoriteCompanyDTO getFavoriteCompanyList() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+        WebClient webClient = webClientBuilder.build();
 
-        VcFavoriteCompanyListDTO favoriteCompanyList = restTemplate.getForObject(companyUrl + "/vc/favorite/list?userIdx={userIdx}", VcFavoriteCompanyListDTO.class, getUser().getId());
-        List<EvaluateStateDTO> evaluateList = restTemplate.getForEntity(investmentUrl + "/evaluate/list/state?companyIdx={companyIdx}", EvaluateStateListDTO.class, getUser().getCompanyIdx()).getBody().getEvaluateState();
+        Mono<VcFavoriteCompanyListDTO> favoriteCompanyList = webClient
+                .get()
+                .uri(companyUrl + "/vc/favorite/list?userIdx={userIdx}", getUser().getId())
+                .retrieve()
+                .bodyToMono(VcFavoriteCompanyListDTO.class);
 
-        return FavoriteCompanyDTO.builder()
-                .vcFavoriteCompanyList(favoriteCompanyList)
-                .evaluateStateList(evaluateList)
-                .build();
+        Mono<EvaluateStateListDTO> evaluateList = webClient
+                .get()
+                .uri(investmentUrl + "/evaluate/list/state?companyIdx={companyIdx}", getUser().getCompanyIdx())
+                .retrieve()
+                .bodyToMono(EvaluateStateListDTO.class);
+
+        return Mono.zip(favoriteCompanyList, evaluateList).map(t -> FavoriteCompanyDTO.builder()
+                .vcFavoriteCompanyList(t.getT1())
+                .evaluateStateList(t.getT2().getEvaluateState())
+                .build()).block();
     }
 
 }
